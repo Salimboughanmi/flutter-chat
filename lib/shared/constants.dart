65a959bfce2f8bceaf8629815dc0d93ed0875bb3
unknown <boughanmi1salim@gmail.com>
2022-12-10 00:32:37 +0100

    application flutter chat with firebase

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1086108385280:web:3daf8ba0e0259d97b598e5";
+  static String apiKey = "AIzaSyAKYIpB_C0CJYGM1qcStZYPiK3IBa2fQeg";
+  static String messagingSenderId = "1086108385280";
+  static String projectId = "chatappflutter-ac37d";
+  final primaryColor = const Color(0xFFee7b64);
+}
